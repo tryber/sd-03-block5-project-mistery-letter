24c47dcf6acb5da1ef59e70b96fd300ecb627108
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-04 17:13:00 -0300

    Changing function to select classes

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-function pickStyle(event) {
-    let style = ["newspaper", "magazine1", "magazine2", ""];
-    choosed = style[Math.floor(Math.random() * 3)];
-    return choosed;
-}
-function pickSize(event) {
-    let size = ["medium", "big", "reallybig", ""];
-    choosed = size[Math.floor(Math.random() * 3)];
-    return choosed;
-}
-function pickRotation(event) {
-    let rotation = ["rotateleft", "rotaterigth", ""];
-    choosed = rotation[Math.floor(Math.random() * 2)];
-    return choosed;
-}
-function pickInclination(event) {
-    let inclination = ["skewleft", "skewright", ""];
-    choosed = inclination[Math.floor(Math.random() * 2)];
-    return choosed;
+function pickClasses(event) {
+  let classes = [['newspaper','magazine1','magazine2','medium'],['big','reallybig', ""],['rotateleft','rotateright', ""],['skewleft','skewright', ""]];
+  let style = classes[0][Math.floor(Math.random() * 2)];
+  let size = classes[1][Math.floor(Math.random() * 2)];
+  let rotation = classes[2][Math.floor(Math.random() * 1)];
+  let inclination = classes[3][Math.floor(Math.random() * 1)];
+  let resultArray = [style, size, rotation, inclination];
+  return resultArray;
 }
 function createLetter(event) {
-    debugger
-    let message = document.getElementById("carta-texto").value;
-    let messageArray = message.split(" ");
-    for (let i = 0; i < messageArray.length; i += 1) {
-        let newSpan = document.createElement("span");
-        document.querySelector("#carta-gerada").appendChild(newSpan);
-        newSpan.innerHTML = messageArray[i];
-        newSpan.classList.add(pickStyle(), pickSize(), pickRotation(), pickInclination());
+  debugger
+  const message = document.getElementById("carta-texto").value;
+  const messageArray = message.split(" ");
+  for (let i = 0; i < messageArray.length; i += 1) {
+    let newSpan = document.createElement("span");
+    document.querySelector("#carta-gerada").appendChild(newSpan);
+    newSpan.innerHTML = messageArray[i];
+    let resultArray = pickClasses();
+    for (let i = 0; i < resultArray.length; i += 1){
+        newSpan.classList.add(resultArray[i]);
     }
+  }
 }
 document.getElementById("criar-carta").addEventListener('click', createLetter);
