dbdd917238719d787339feeed137b60693ed8076
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-04 00:37:51 -0300

    teste

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const rotacao = ['rotateleft', 'rotateright'];
 const inclinacao = ['skewleft', 'skewright'];
 
 function classeAleatoria() {
-    let estiloA = Math.round(Math.random() * (estilo.length - 1));
-    let tamanhoA = Math.round(Math.random() * (tamanho.length - 1));
-    let rotacaoA = Math.round(Math.random() * (rotacao.length - 1));
-    let inclinacaoA = Math.round(Math.random() * (rotacao.length - 1));
-    let classe = estilo[estiloA] + " " + tamanho[tamanhoA] + " " + rotacao[rotacaoA] + " " + inclinacao[inclinacaoA];
-    return classe; 
+  const estiloA = Math.round(Math.random() * (estilo.length - 1));
+  const tamanhoA = Math.round(Math.random() * (tamanho.length - 1));
+  const rotacaoA = Math.round(Math.random() * (rotacao.length - 1));
+  const inclinacaoA = Math.round(Math.random() * (rotacao.length - 1));
+  const classe = estilo[estiloA] + ' ' + tamanho[tamanhoA] + ' ' + rotacao[rotacaoA] + ' ' + inclinacao[inclinacaoA];
+  return classe;
 }
 
 const inputCarta = document.querySelector('#carta-texto');
@@ -19,36 +19,36 @@ const contador = document.querySelector('#carta-contador');
 const botaoLimpa = document.querySelector('#limpa-carta');
 
 function addCarta() {
-    let arrayCarta = inputCarta.value.split(' ');
-    for (let i = 0; i < arrayCarta.length; i += 1) {
-        const span = document.createElement('span');
-        span.className = classeAleatoria();
-        span.innerHTML = arrayCarta[i];
-        pCarta.appendChild(span);
-    }
-    contador.innerHTML = 'Total de palavras: ' + arrayCarta.length;
-    let spanCriado = document.querySelectorAll('span');
-    for (let j = 0; j < arrayCarta.length; j += 1) {
-        novaClasse(spanCriado, j);
-    }
+  let arrayCarta = inputCarta.value.split(' ');
+  for (let i = 0; i < arrayCarta.length; i += 1) {
+    const span = document.createElement('span');
+    span.className = classeAleatoria();
+    span.innerHTML = arrayCarta[i];
+    pCarta.appendChild(span);
+  }
+  contador.innerHTML = arrayCarta.length;
+  let spanCriado = document.querySelectorAll('span');
+  for (let j = 0; j < arrayCarta.length; j += 1) {
+    novaClasse(spanCriado, j);
+  }
 }
 
 botaoCarta.addEventListener('click', addCarta);
 
 function limpaCarta() {
-    let arrayCarta = inputCarta.value.split(' ');
-    for (let i = arrayCarta.length - 1; i >= 0; i -= 1) {
-        spanCriado = document.querySelectorAll('span');
-        pCarta.removeChild(spanCriado[i]);
-    }
-    contador.innerHTML = '';
-    inputCarta.value = '';
+  let arrayCarta = inputCarta.value.split(' ');
+  for (let i = arrayCarta.length - 1; i >= 0; i -= 1) {
+    spanCriado = document.querySelectorAll('span');
+    pCarta.removeChild(spanCriado[i]);
+  }
+  contador.innerHTML = '';
+  inputCarta.value = '';
 }
 
 botaoLimpa.addEventListener('click', limpaCarta);
 
 function novaClasse(spanCriado, j) {
-    spanCriado[j].addEventListener('click', function () {
-        spanCriado[j].className = classeAleatoria();
-    });
+  spanCriado[j].addEventListener('click', function() {
+    spanCriado[j].className = classeAleatoria();
+  });
 }
