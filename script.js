30f47f535ffd0ddb0bb9044f9f1d5ec8a219fbbb
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-04 00:22:51 -0300

    add classe aos spans e botao de apgar carta

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-const inputCarta = document.getElementById('carta-texto');
-const botaoCarta = document.getElementById('criar-carta');
-const pCarta = document.getElementById('carta-gerada');
 const estilo = ['newspaper', 'magazine1', 'magazine2'];
 const tamanho = ['medium', 'big', 'reallybig'];
 const rotacao = ['rotateleft', 'rotateright'];
-const inclinacao = ['skewleft', 'skewright']
+const inclinacao = ['skewleft', 'skewright'];
+
+function classeAleatoria() {
+    let estiloA = Math.round(Math.random() * (estilo.length - 1));
+    let tamanhoA = Math.round(Math.random() * (tamanho.length - 1));
+    let rotacaoA = Math.round(Math.random() * (rotacao.length - 1));
+    let inclinacaoA = Math.round(Math.random() * (rotacao.length - 1));
+    let classe = estilo[estiloA] + " " + tamanho[tamanhoA] + " " + rotacao[rotacaoA] + " " + inclinacao[inclinacaoA];
+    return classe; 
+}
+
+const inputCarta = document.querySelector('#carta-texto');
+const botaoCarta = document.querySelector('#criar-carta');
+const pCarta = document.querySelector('#carta-gerada');
+const contador = document.querySelector('#carta-contador');
+const botaoLimpa = document.querySelector('#limpa-carta');
 
 function addCarta() {
     let arrayCarta = inputCarta.value.split(' ');
-    console.log(arrayCarta);
     for (let i = 0; i < arrayCarta.length; i += 1) {
         const span = document.createElement('span');
+        span.className = classeAleatoria();
         span.innerHTML = arrayCarta[i];
         pCarta.appendChild(span);
     }
+    contador.innerHTML = 'Total de palavras: ' + arrayCarta.length;
+    
 }
 
 botaoCarta.addEventListener('click', addCarta);
+
+function limpaCarta() {
+    let arrayCarta = inputCarta.value.split(' ');
+    for (let i = arrayCarta.length - 1; i >= 0; i -= 1) {
+        let spanCriado = document.querySelectorAll('span')[i];
+        pCarta.removeChild(spanCriado);
+    }
+    contador.innerHTML = '';
+    inputCarta.value = '';
+}
+
+botaoLimpa.addEventListener('click', limpaCarta);
