487230b70662e2c2341547b7b2349348fb995e22
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-04 17:26:48 -0300

    1-bonuso feito, 2-bonus corrigido, teste3-obrigatorio

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function choseClass() {
   let classes = '';
   let number = 0;
   const pEstilo = document.getElementById('armazem-estilo').classList;
-  
+
   number = Math.floor(Math.random() * 3);
   classes += `${pEstilo[number]}`;
 
@@ -17,39 +17,41 @@ function choseClass() {
   const pInclinacao = document.getElementById('armazem-inclinacao').classList;
   number = Math.floor(Math.random() * 2);
   classes += ` ${pInclinacao[number]}`;
-  console.log(classes);
+
   return classes;
 }
 
 function showNumberCards(counter) {
-  const p = document.getElementById('contador');
+  const p = document.getElementById('carta-contador');
   p.innerText = counter;
 }
 
-function makeCards () {
+function reClasser() {
+  event.target.className = choseClass();
+}
+
+function makeCards() {
   const inputSentence = document.getElementById('carta-texto').value;
   const words = inputSentence.split(' ');
   const paragrafo = document.getElementById('carta-gerada');
 
   let counter = 0;
-  for (let word of words) {
+  for (const word of words) {
     counter += 1;
-    let span = document.createElement('span');
+    const span = document.createElement('span');
+
     span.innerText = word;
     span.className = choseClass();
-    paragrafo.appendChild(span);
+    span.addEventListener('click', reClasser);
 
+    paragrafo.appendChild(span);
   }
   showNumberCards(counter);
 }
 
 function LOADED() {
-  let buttonMakeCards = document.getElementById('criar-carta');
+  const buttonMakeCards = document.getElementById('criar-carta');
   buttonMakeCards.addEventListener('click', makeCards);
-
-  let classObject = document.styleSheets[0].rules// || document.styleSheets[0].cssRules;
-  
-  
 }
 
 window.onload = LOADED();
