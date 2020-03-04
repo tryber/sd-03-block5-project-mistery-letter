7b08758e19bf8ba3526ebdcde1508a690a72ce71
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-04 15:24:21 -0300

    falta classes

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+function choseClass(n) {
+  const randomclass = Math.floor(Math.random() * n);
+  return randomclass;
+}
+
+function makeCards () {
+  const inputSentence = document.getElementById('carta-texto').value;
+  const words = inputSentence.split(' ');
+  const paragrafo = document.getElementById('carta-gerada');
+
+  let counter = 0;
+  for (let word of words) {
+    counter += 1;
+    let span = document.createElement('span');
+    span.innerText = words[word];
+    paragrafo.appendChild(span);
+
+  }
+}
+
 function LOADED() {
   let buttonMakeCards = document.getElementById('criar-carta');
-  buttonMakeCards.addEventListener('click', makeCards)
+  buttonMakeCards.addEventListener('click', makeCards);
 }
 
 window.onload = LOADED();
