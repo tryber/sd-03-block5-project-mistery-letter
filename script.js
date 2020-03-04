c10dfcbfa8f144ad85af5fb8babca0b39103962d
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-03 23:19:30 -0300

    requisito 1 a 3

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputCarta = document.getElementById('carta-texto');
+const botaoCarta = document.getElementById('criar-carta');
+const pCarta = document.getElementById('carta-gerada');
+
+function addCarta() {
+    let arrayCarta = inputCarta.value.split(" ");
+    console.log(arrayCarta);
+    for (let i = 0; i < arrayCarta.length; i += 1) {
+        const span = document.createElement('span');
+        span.innerHTML = arrayCarta[i];
+        pCarta.appendChild(span);
+    }
+}
+
+botaoCarta.addEventListener('click', addCarta);
