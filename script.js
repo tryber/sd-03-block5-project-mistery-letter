dae7784036e9cb84ac0808d054d8dd5e90a243ba
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-04 17:57:36 -0300

    Adicionando evento no botao para criar carta

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("criar-carta").addEventListener("click", () => {
+  let palavras = document.getElementById("carta-texto").value.split(' ');
+  palavras.forEach(element => {
+    let span = document.createElement("span")
+    span.textContent = element
+    document.getElementById("carta-gerada").appendChild(span)
+  });
+});
