722e7b3eb5b8fc2137561d7af3962dbac68a7245
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-04 18:03:33 -0300

    teste

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
+
 document.getElementById("criar-carta").addEventListener("click", () => {
   let palavras = document.getElementById("carta-texto").value.split(' ');
   palavras.forEach(element => {
     let span = document.createElement("span")
-    span.textContent = element
+    span.textContent = element + ' '
     document.getElementById("carta-gerada").appendChild(span)
   });
 });
