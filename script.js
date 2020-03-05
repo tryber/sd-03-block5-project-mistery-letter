740bfd3c4af2e40c84c81afa47cf0f942bde1c18
fabemiliano <fab.emiliano@gmail.com>
2020-03-04 23:12:05 -0300

    adding classes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-let generateLetter = document.getElementById("criar-carta")
+    let generateLetter = document.getElementById("criar-carta")
     generateLetter.addEventListener('click', createLetter); 
     
     function createLetter(){
@@ -7,10 +6,22 @@ let generateLetter = document.getElementById("criar-carta")
         let textBox = textoInput.split(" ")
     
         for (let i = 0; i < textBox.length; i++){
+            
             let word = document.createElement("span")
             document.getElementById("carta-gerada").appendChild(word)
             word.innerHTML = textBox[i]
+
+
+            let classListEstilo = ["newspaper","magazine1","magazine2"]
+            let classListSize = ["medium","big","reallybig"]
+            let classListRotation = ["rotateleft","rotateright"]
+            let classListAngle = ["skewleft","skewright"]
+            classRandon1 = classListEstilo[Math.floor(Math.random() * classListEstilo.length)]
+            classRandon2 = classListSize[Math.floor(Math.random() * classListSize.length)]
+            classRandon3 = classListRotation[Math.floor(Math.random() * classListRotation.length)]
+            classRandon4 = classListAngle[Math.floor(Math.random() * classListAngle.length)]
+            word.classList.add(classRandon1, classRandon2, classRandon3, classRandon4)
         }
     }
-    
+
 
