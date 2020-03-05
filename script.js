5a83dcd417e23d01098e490885756f12d99c02e8
ilan274 <ilan.herbach@gmail.com>
2020-03-04 22:37:09 -0300

    Classes added

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Get variables
-const createButton = document.getElementById("criar-carta");
+const createButton = document.getElementById('criar-carta');
 
 // Functions
 function createFunction() {
-  let separadas = document.getElementById("carta-texto").value.split(" ");
+  const separadas = document.getElementById('carta-texto').value.split(' ');
   for (let i = 0; i < separadas.length; i += 1) {
-    let createSpan = document.createElement("span");
-    let cartaGerada = document.getElementById("carta-gerada");
+    const createSpan = document.createElement('span');
+    const cartaGerada = document.getElementById('carta-gerada');
     createSpan.innerHTML = separadas[i];
     cartaGerada.appendChild(createSpan);
   }
 }
 
 // Event listeners
-createButton.addEventListener("click", createFunction);
+createButton.addEventListener('click', createFunction);
