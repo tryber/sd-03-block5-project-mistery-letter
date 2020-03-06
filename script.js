1c8e813a3fa07939a880c3264a569756b4e783b8
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-06 18:43:19 -0300

    Teste bot

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 window.onload = () => {
-  const grupoEstilo = ["newspaper", "magazine1", "magazine2"]
-  const grupoTamanho = ['medium', 'big', 'reallyBig']
-  const grupoRotacao = ['rotateleft', 'rotateright']
-  const grupoInclinacao = ['skewleft', 'skewright']
+  const grupoEstilo = ["newspaper", "magazine1", "magazine2"];
+  const grupoTamanho = ['medium', 'big', 'reallyBig'];
+  const grupoRotacao = ['rotateleft', 'rotateright'];
+  const grupoInclinacao = ['skewleft', 'skewright'];
+  const randomNumber = (max) => Math.trunc((max) * Math.random());
+  const randomClass = () => `${grupoEstilo[randomNumber(grupoEstilo.length - 1)]} ${grupoTamanho[randomNumber(grupoTamanho.length - 1)]} ${grupoRotacao[randomNumber(grupoRotacao.length - 1)]} ${grupoInclinacao[randomNumber(grupoInclinacao.length - 1)]}`;
 
   document.getElementById("criar-carta").addEventListener("click", () => {
-    let randomNumber = (max) => Math.trunc((max) * Math.random());
-    let randomClass = () => `${grupoEstilo[randomNumber(grupoEstilo.length)]} ${grupoTamanho[randomNumber(grupoTamanho.length)]} ${grupoRotacao[randomNumber(grupoRotacao.length)]} ${grupoInclinacao[randomNumber(grupoInclinacao.length)]}`
-
     document.getElementById("carta-gerada").innerHTML = ''
     let palavras = document.getElementById("carta-texto").value.split(' ');
     document.getElementById("carta-contador").textContent = palavras.length;
     palavras.forEach(element => {
-      let span = document.createElement("span")
-      span.textContent = element
-      span.classList = randomClass()
-      span.addEventListener('click', (event) => event.target.classList = randomClass())
-      document.getElementById("carta-gerada").appendChild(span)
-
+      let span = document.createElement("span");
+      span.textContent = element;
+      span.classList = randomClass();
+      span.addEventListener('click', (event) => event.target.classList = randomClass());
+      document.getElementById("carta-gerada").appendChild(span);
     });
   });
 }
