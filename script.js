76143898be2570caf4733ecd9fd393dc51596e1e
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-06 18:29:04 -0300

    Teste bot

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 window.onload = () => {
-  const randomNumber = (max) => Math.floor((max) * Math.random());
+  const randomNumber = (max) => Math.trunc((max) * Math.random());
   const grupoEstilo = ["newspaper", "magazine1", "magazine2"]
   const grupoTamanho = ['medium', 'big', 'reallyBig']
   const grupoRotacao = ['rotateleft', 'rotateright']
   const grupoInclinacao = ['skewleft', 'skewright']
-  const randomClass = () => `${grupoEstilo[randomNumber(grupoEstilo.length - 1)]} ${grupoTamanho[randomNumber(grupoTamanho.length - 1)]} ${grupoRotacao[randomNumber(grupoRotacao.length)]} ${grupoInclinacao[randomNumber(grupoInclinacao.length)]}`
+  const randomClass = () => `${grupoEstilo[randomNumber(grupoEstilo.length)]} ${grupoTamanho[randomNumber(grupoTamanho.length)]} ${grupoRotacao[randomNumber(grupoRotacao.length)]} ${grupoInclinacao[randomNumber(grupoInclinacao.length)]}`
 
   document.getElementById("criar-carta").addEventListener("click", () => {
-    console.log(randomNumber(3))
-    console.log(grupoEstilo.length)
     document.getElementById("carta-gerada").innerHTML = ''
     let palavras = document.getElementById("carta-texto").value.split(' ');
     document.getElementById("carta-contador").textContent = palavras.length;
