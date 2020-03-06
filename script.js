c878647e0da292c739379ed23eb7f4958ca3a3b5
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-06 18:09:42 -0300

    Teste bot

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-// Cria número de 0 a max
-const randomNumber = (max) => 0 + Math.floor((max - 0) * Math.random());
+window.onload = () => {
+  const randomNumber = (max) => Math.floor((max + 1) * Math.random());
+  const grupoEstilo = ["newspaper", "magazine1", "magazine2"]
+  const grupoTamanho = ['medium', 'big', 'reallyBig']
+  const grupoRotacao = ['rotateleft', 'rotateright']
+  const grupoInclinacao = ['skewleft', 'skewright']
+  const randomClass = () => `${grupoEstilo[randomNumber(grupoEstilo.length - 1)]} ${grupoTamanho[randomNumber(grupoTamanho.length - 1)]} ${grupoRotacao[randomNumber(grupoRotacao.length)]} ${grupoInclinacao[randomNumber(grupoInclinacao.length)]}`
 
-const grupoEstilo = ["newspaper", "magazine1", "magazine2"]
-const grupoTamanho = ['medium', 'big', 'reallyBig']
-const grupoRotacao = ['rotateleft', 'rotateright']
-const grupoInclinacao = ['skewleft', 'skewright']
-
-const randomClass = () => `${grupoEstilo[randomNumber(grupoEstilo.length - 1)]} ${grupoTamanho[randomNumber(grupoTamanho.length - 1)]} ${grupoRotacao[randomNumber(grupoRotacao.length - 1)]} ${grupoInclinacao[randomNumber(grupoInclinacao.length - 1)]}`
-
-console.log(randomClass())
-document.getElementById("criar-carta").addEventListener("click", () => {
-  document.getElementById("carta-gerada").innerHTML = ''
-  let palavras = document.getElementById("carta-texto").value.split(' ');
-  document.getElementById("carta-contador").textContent = palavras.length;
-  palavras.forEach(element => {
-    let span = document.createElement("span")
-    span.textContent = element
-    span.classList = randomClass()
-    span.addEventListener('click', (event) => event.target.classList = randomClass())
-    document.getElementById("carta-gerada").appendChild(span)
+  document.getElementById("criar-carta").addEventListener("click", () => {
+    console.log(randomNumber(3))
+    document.getElementById("carta-gerada").innerHTML = ''
+    let palavras = document.getElementById("carta-texto").value.split(' ');
+    document.getElementById("carta-contador").textContent = palavras.length;
+    palavras.forEach(element => {
+      let span = document.createElement("span")
+      span.textContent = element
+      span.classList = randomClass()
+      span.addEventListener('click', (event) => event.target.classList = randomClass())
+      document.getElementById("carta-gerada").appendChild(span)
+    });
   });
-});
+}
