3a8365f1893fc87b326d607d08108b6a4bf6ff1a
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-07 18:12:26 -0300

    Adjusting code climate

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-function countWords(event){
-  const message = document.getElementById("carta-texto").value;
-  const messageArray = message.split(" ");
+function countWords() {
+  const message = document.getElementById('carta-texto').value;
+  const messageArray = message.split('');
   const numberWords = messageArray.length;
-  document.getElementById("carta-contador").innerHTML = numberWords;
+  document.getElementById('carta-contador').innerHTML = numberWords;
 }
-function deletePrevious(event){
-  if (document.getElementById("carta-gerada") != null){
-    document.getElementById("carta-gerada").innerHTML = '';
+function deletePrevious() {
+  if (document.getElementById('carta-gerada') != null) {
+    document.getElementById('carta-gerada').innerHTML = '';
   }
 }
-function pickClasses(event) {
-  let classes = [['newspaper','magazine1','magazine2','medium'],['big','reallybig'],['rotateleft','rotateright'],['skewleft','skewright']];
-  let style = classes[0][Math.trunc(Math.random() * 3)];
-  let size = classes[1][Math.trunc(Math.random() * 3)];
-  let rotation = classes[2][Math.trunc(Math.random() * 2)];
-  let inclination = classes[3][Math.trunc(Math.random() * 2)];
-  let resultArray = [style, size, rotation, inclination];
+function pickClasses() {
+  const classes = [['newspaper' , 'magazine1' , 'magazine2' , 'medium'] , ['big' , 'reallybig'] , ['rotateleft' , 'rotateright'] , ['skewleft' , 'skewright']];
+  const style = classes[0][Math.trunc(Math.random() * 3)];
+  const size = classes[1][Math.trunc(Math.random() * 3)];
+  const rotation = classes[2][Math.trunc(Math.random() * 2)];
+  const inclination = classes[3][Math.trunc(Math.random() * 2)];
+  const resultArray = [style, size, rotation, inclination];
   return resultArray;
 }
-function createLetter(event) {
+function changeClasses(event) {
+  event.target.className = ('');
+  let resultArray = pickClasses();
+  for (let i = 0; i < resultArray.length; i += 1) {
+    event.target.classList.add(resultArray[i]);
+  }
+}
+function createLetter() {
   deletePrevious();
-  const message = document.getElementById("carta-texto").value;
-  const messageArray = message.split(" ");
+  const message = document.getElementById('carta-texto').value;
+  const messageArray = message.split(' ');
   for (let i = 0; i < messageArray.length; i += 1) {
-    let newSpan = document.createElement("span");
-    document.querySelector("#carta-gerada").appendChild(newSpan);
+    const newSpan = document.createElement('span');
+    document.querySelector('#carta-gerada').appendChild(newSpan);
     newSpan.addEventListener('click', changeClasses);
     newSpan.innerHTML = messageArray[i];
-    let resultArray = pickClasses();
-    for (let i = 0; i < resultArray.length; i += 1){
-        newSpan.classList.add(resultArray[i]);
+    const resultArray = pickClasses();
+    for (let k = 0; k < resultArray.length; k += 1) {
+      newSpan.classList.add(resultArray[i]);
     }
   }
   countWords();
 }
-function changeClasses(event){
-  event.target.className = ("");
-  let resultArray = pickClasses();
-    for (let i = 0; i < resultArray.length; i += 1){
-      event.target.classList.add(resultArray[i]);
-    }
-}
-document.getElementById("criar-carta").addEventListener('click', createLetter);
+document.getElementById('criar-carta').addEventListener('click', createLetter);
