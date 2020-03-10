cdade179cda8b3e996c545af34c13d153bf16b2a
Clayton Pereira <cpereiramt@gmail.com>
2020-03-10 16:01:03 -0400

    Corrigindo erros nos requisitos

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ botao_criar.addEventListener('click', function(){
 });
 
 function estilizarTexto(){
+    let classes = ['newspaper','magazine1','magazine2','medium','big',
+      'reallybig','rotateleft','rotateright','skewleft','skewright' ];
     let inputText = document.getElementById('carta-texto');
     let palavras = inputText.value.split(" ");
 
     for(let i in palavras){
         let spanElement = document.createElement('span') ;
         spanElement.innerHTML = palavras[i];
-        
+
         console.log(spanElement);
         document.getElementById('carta-gerada').appendChild(spanElement);
     }
