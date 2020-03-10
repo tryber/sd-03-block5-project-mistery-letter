136d187d3a004a0e48291061c82fb8143cfd4c75
Clayton Pereira <cpereiramt@gmail.com>
2020-03-10 18:54:25 -0400

    Corrigindo erros nos requisitos

--- script.js
+++ script.js
@@ -3,12 +3,11 @@
 window.onload = function(){
   
 }
-
  let estilo =  ['newspaper','magazine1','magazine2'];
  let tamanho =  ['medium','big', 'reallybig'];
  let rotacao =  ['rotateleft','rotateright'];
  let inclinacao = ['skewleft','skewright'];
- 
+ let spanElement ;
 botao_criar = document.getElementById('criar-carta');
 botao_criar.addEventListener('click', function(){
     estilizarTexto();
@@ -23,14 +22,23 @@ function estilizarTexto(){
     document.getElementById('carta-gerada').innerHTML = "";
     for(let i in palavras){
 
-        let spanElement = document.createElement('span') ;
+        spanElement = document.createElement('span') ;
         spanElement.classList.add(estilo[Math.floor(Math.random() * 3)],
                                   tamanho[Math.floor(Math.random() * 3)],
                                   rotacao[Math.floor(Math.random() * 2)],
                                   inclinacao[Math.floor(Math.random() * 2)]);
         spanElement.innerHTML = palavras[i];
+       
         
         document.getElementById('carta-gerada').appendChild(spanElement);
+        spanElement.addEventListener('click', function(event){
+            event.target.classList.remove();
+            event.target.classList.add(estilo[Math.floor(Math.random() * 3)],
+            tamanho[Math.floor(Math.random() * 3)],
+            rotacao[Math.floor(Math.random() * 2)],
+            inclinacao[Math.floor(Math.random() * 2)]);
+       });
     }
+   
      
 }
