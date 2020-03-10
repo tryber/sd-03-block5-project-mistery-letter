3297e495ebf2aeb42ed856d655c2a57bea57dd3c
Gabriel Lucas <dreadpool38@gmail.com>
2020-03-10 20:03:39 -0300

    adicionado bonus

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function writeLetter(letter) {
 let container = document.getElementById("carta-gerada");
 let carta = document.querySelector(letter.dataset.ref).value.split(" ");
 
+container.innerHTML = "";
+
 for (let palavra of carta){
+  if (palavra == ""){
+    return
+  }
+
   let word = document.createElement('span');
   word.innerHTML = palavra;
 
@@ -20,6 +26,38 @@ for (let palavra of carta){
 
   }
 
+  word.addEventListener('click',setNewStyle);
+  word.style.cursor = "pointer";
+
   container.appendChild(word);
 }
+
+setContador()
+}
+
+function setNewStyle(e) {
+  e.target.setAttribute("class",'')
+
+  let classesNumber = Math.floor(Math.random()*3)+2;
+
+  let grupos = [
+    [`newspaper`, `magazine1`, `magazine2`],
+    [`medium`, `big`, `reallybig`],
+    [`rotateleft`, `rotateright`],
+    [`skewleft`, `skewright`]
+  ]
+
+  for (let i = 0; i < classesNumber ; i++) {
+    e.target.classList.add(grupos[i][Math.floor(Math.random()* grupos[i].length) ]);
+
+  }
+
+
+}
+
+function setContador() {
+  let contador = document.querySelector("#carta-contador")
+  let contadorNumero = document.querySelector("#carta-gerada").childElementCount;
+  contador.innerHTML = contadorNumero;
+
 }
