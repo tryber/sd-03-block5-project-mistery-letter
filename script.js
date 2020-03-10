7de9618d87dd3363de384ef96e429e85d798a94e
wscatao <wscatao@gmail.com>
2020-03-10 17:47:38 -0300

    Atualizado o texto do paragrafo

--- script.js
+++ script.js
@@ -8,56 +8,56 @@ const contador = document.querySelector('#carta-contador');
 // Funções
 
 function contarPalavras() {
-    const textoDigitado = inputText.value.split(" ");
-    contador.innerHTML = "Quantidade de palavras digitadas: " + textoDigitado.length;
+  const textoDigitado = inputText.value.split(" ");
+  contador.innerHTML = textoDigitado.length;
 }
 
 
 function adicionarClasse(span) {
-    const estilo = ['newspaper', 'magazine1', 'magazine2'];
-    const tamanho = ['medium','big','reallybig'];
-    const transform = [rotacao = ['rotateleft', 'rotateright'],inclinacao = ['skewleft', 'skewright']];
-    const aleatoriedade = [estilo,tamanho,transform[Math.floor(Math.random() * 2)]];
-    if (span.classList) {
-        span.removeAttribute('class');
+  const estilo = ['newspaper', 'magazine1', 'magazine2'];
+  const tamanho = ['medium', 'big', 'reallybig'];
+  const transform = [rotacao = ['rotateleft', 'rotateright'], inclinacao = ['skewleft', 'skewright']];
+  const aleatoriedade = [estilo, tamanho, transform[Math.floor(Math.random() * 2)]];
+  if (span.classList) {
+    span.removeAttribute('class');
+  }
+  for (let i = 0; i < aleatoriedade.length; i += 1) {
+    if (i < 2) {
+      span.classList.add(aleatoriedade[i][Math.floor(Math.random() * 3)]);
+    } else {
+      span.classList.add(aleatoriedade[i][Math.floor(Math.random() * 2)]);
     }
-    for (let i = 0; i < aleatoriedade.length; i += 1) {
-        if (i < 2) {
-            span.classList.add(aleatoriedade[i][Math.floor(Math.random() * 3)]);
-        } else {
-            span.classList.add(aleatoriedade[i][Math.floor(Math.random() * 2)]);
-        }
-    }
+  }
 }
 
 function addCustomClass(span) {
-    span.addEventListener('click', function(){
-        adicionarClasse(span);
-    })
+  span.addEventListener('click', function () {
+    adicionarClasse(span);
+  })
 }
 
 
 function montarParagrafo(item) {
-    const span = document.createElement('span');
-    span.innerHTML = item;
-    adicionarClasse(span);
-    addCustomClass(span);
-    paragrafo.appendChild(span);
+  const span = document.createElement('span');
+  span.innerHTML = item;
+  adicionarClasse(span);
+  addCustomClass(span);
+  paragrafo.appendChild(span);
 }
 
 function gerarParagrafo() {
-    if (paragrafo) {
-        paragrafo.innerHTML = '';
-    }
-    const textoDigitado = inputText.value.split(" ");
-    textoDigitado.forEach(montarParagrafo);
+  if (paragrafo) {
+    paragrafo.innerHTML = '';
+  }
+  const textoDigitado = inputText.value.split(" ");
+  textoDigitado.forEach(montarParagrafo);
 }
 
-function teste () {
-    console.log('funciona');
+function teste() {
+  console.log('funciona');
 }
 
 //  event listener
 
-botao.addEventListener('click',gerarParagrafo);
+botao.addEventListener('click', gerarParagrafo);
 botao.addEventListener('click', contarPalavras);
