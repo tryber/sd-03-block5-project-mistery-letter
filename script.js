ac3a9be1fe046c042d33731af0132d2ba38480d5
Clayton Pereira <cpereiramt@gmail.com>
2020-03-10 17:07:56 -0400

    Corrigindo erros nos requisitos

--- script.js
+++ script.js
@@ -14,18 +14,20 @@ botao_criar.addEventListener('click', function(){
 });
 
 function estilizarTexto(){
-    let classes = [['newspaper','magazine1','magazine2'],['medium','big',
-      'reallybig'],['rotateleft','rotateright'],['skewleft','skewright']];
+    let classes = (['newspaper','magazine1','magazine2'],['medium','big',
+      'reallybig'],['rotateleft','rotateright'],['skewleft','skewright']);
     let inputText = document.getElementById('carta-texto');
     let palavras = inputText.value.split(" ");
 
-    console.log(classes[Math.floor(Math.random() * 4)][Math.floor(Math.random() * classes.length)]);
-
+   /*  let class1 = classes[Math.floor(Math.random() * 4)][Math.floor(Math.random() * classes.length)];
+       let class2 = classes[Math.floor(Math.random() * 4)][Math.floor(Math.random() * classes.length)];
+     */
+    
     for(let i in palavras){
         let spanElement = document.createElement('span') ;
         spanElement.innerHTML = palavras[i];
 
-        console.log(spanElement);
+        console.log(classes);
         document.getElementById('carta-gerada').appendChild(spanElement);
     }
      
