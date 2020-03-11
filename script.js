8a7b927b2662e6e1f600b135fea5834ba8b1609b
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-11 10:29:12 -0300

    create element span

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const botao = document.getElementById('criar-carta');
+const carta = document.getElementById('carta-gerada');
+const texto = document.getElementById('carta-texto');
+
 botao.addEventListener('click', function criarCarta() {
-  alert('Carta criada!');
+  const span = document.createElement('span');
+  carta.appendChild(span);
+
+  span.innerText = texto.value + ' ';
 });
