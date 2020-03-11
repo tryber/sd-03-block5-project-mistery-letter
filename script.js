5c572843885115a66bdcc2c68d21f6c86e338620
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-11 11:30:04 -0300

    create span for word

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const carta = document.getElementById('carta-gerada');
 const texto = document.getElementById('carta-texto');
 
 botao.addEventListener('click', function criarCarta() {
-  for (let i=0; i<7; i++){
+  const frase = texto.value;
+  var frasesp = frase.split(' ');
+  
+  for (let i in frasesp){
     const span = document.createElement('span');
     carta.appendChild(span);
-    span.innerText = texto.value + ' ';
+    span.innerText = frasesp[i] + ' ';
   }
 });
