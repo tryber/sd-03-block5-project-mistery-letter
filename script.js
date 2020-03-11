9fa27d830abd029d2a4a87da55817d702707062b
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-11 10:42:29 -0300

    try again lol 10.42

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const carta = document.getElementById('carta-gerada');
 const texto = document.getElementById('carta-texto');
 
 botao.addEventListener('click', function criarCarta() {
-  const span = document.createElement('span');
-  carta.appendChild(span);
-
-  span.innerText = texto.value + ' ';
+  for (let i=0; i<7; i++){
+    const span = document.createElement('span');
+    carta.appendChild(span);
+    span.innerText = texto.value + ' ';
+  }
 });
