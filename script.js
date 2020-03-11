28f361b25216ede9f19e52e2c93017ff65965ee9
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-11 12:37:08 -0300

    style fix

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ const texto = document.getElementById('carta-texto');
 
 botao.addEventListener('click', function criarCarta() {
   const frase = texto.value;
-  let fraseSplit = frase.split(' ');
+  const fraseSplit = frase.split(' ');
+  const estilo = ['newspaper', 'magazine1', 'magazine2'];
+  const tamanho = ['medium', 'big', 'reallybig'];
+  const rotacao = ['rotateleft', 'rotateright', 'rotateleft'];
+  const inclina = ['skewleft', 'skewright', 'skewleft'];
+  const grupos = [estilo, tamanho, rotacao, inclina];
 
   for (const i in fraseSplit) {
     const span = document.createElement('span');
     carta.appendChild(span);
     span.innerText = fraseSplit[i];
+    
+    const gp = Math.floor(Math.random() * 3);
+    const cl = Math.floor(Math.random() * 2);
+    span.classList.add(grupos[gp][cl]);
   }
 });
