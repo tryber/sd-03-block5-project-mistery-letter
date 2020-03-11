332468af82e6d19bcf6cfb1dd2fcfb6e59cdd7f6
ilan274 <ilan.herbach@gmail.com>
2020-03-11 10:36:17 -0300

    Project requirements

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 // Get variables
 const createButton = document.getElementById('criar-carta');
 
-// Functions
+function estilo() {
+  const classes = ['newspaper', 'magazine1', 'magazine2'];
+  return classes[Math.floor(Math.random() * 3)];
+}
+
+function tamanho() {
+  const classes = ['medium', 'big', 'reallybig'];
+  return classes[Math.floor(Math.random() * 3)];
+}
+
+function rotacao() {
+  const classes = ['rotateleft', 'rotateright'];
+  return classes[Math.floor(Math.random() * 2)];
+}
+
+function inclinacao() {
+  const classes = ['skewleft', 'skewright'];
+  return classes[Math.floor(Math.random() * 2)];
+}
+
+/*function classesAleatorias() {
+  const style = classes[Math.floor(Math.random() * 2)];
+  const size = classes[1][Math.floor(Math.random() * 2)];
+  const rotation = classes[2][Math.floor(Math.random() * 1)];
+  const inclination = classes[[Math.floor(Math.random() * 1)]
+  resultArray = [style];
+  
+  return resultArray.join(',').replace(/,/g, " ");
+}*/
+
 function createLetter() {
   const separadas = document.getElementById('carta-texto').value.split(' ');
   for (let i = 0; i < separadas.length; i += 1) {
@@ -9,6 +38,7 @@ function createLetter() {
     const cartaGerada = document.getElementById('carta-gerada');
     createSpan.innerHTML = separadas[i];
     cartaGerada.appendChild(createSpan);
+    createSpan.classList.add(estilo(), tamanho(), rotacao(), inclinacao());
   }
 }
 
